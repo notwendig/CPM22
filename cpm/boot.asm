3fa2ff956f823b4f4e19b4179dfdc85844d982ad
Jürgen Willi Sievers <JSievers@NadiSoft.de>
2026-08-14 00:07:30 +0200

    Use Z80 as git submodule

--- cpm/boot.asm
+++ cpm/boot.asm
@@ -3,57 +3,57 @@
 ;       CP/M 2.2 boot-loader for Z80-Simulator
 ;	Copyright (C) 1998-2012 by Juergen Sievers
 ;
-        include cpm.inc
+		include cpm.inc
 
-                ORG     0           ; mem base of cp/m
+		ORG     0           ; mem base of cp/m
 ;
 ;       begin the load operation
 ;
 COLD:	DI
-                LD		SP, TBUFF
-                LD		HL, BOOTMSG
-                LD		A,FF
-                JR		MSG
+		LD		SP, TBUFF
+		LD		HL, BOOTMSG
+		LD		A,FF
+		JR		MSG
 
 BERR:	LD		HL,HALTMSG
-                LD      A,BEEP
+		LD      A,BEEP
 
 MSG:	OUT		(CONDAT), A
-                LD		A, (HL)
-                INC		HL
-                CP		'$'
-                JR		NZ, MSG
+		LD		A, (HL)
+		INC		HL
+		CP		'$'
+		JR		NZ, MSG
 
-                LD      BC,BOFFST		; b=track, c=sector
-                LD      D, BSECTS       ; d=# sectors to load
-                LD      HL,BIOS         ; BIOS start address
-                XOR		A               ; select drive A
-                OUT		(DPBL),A
-                OUT		(DPBH),A		; set default IBM 8" Disk 26 sector/track
-                OUT     (FDCD),A
+		LD      BC,BOFFST		; b=track, c=sector
+		LD      D, BSECTS       ; d=# sectors to load
+		LD      HL,BIOS         ; BIOS start address
+		XOR		A               ; select drive A
+		OUT		(DPBL),A
+		OUT		(DPBH),A		; set default IBM 8" Disk 26 sector/track
+		OUT     (FDCD),A
 ;
 ;       load the next sector
 ;
 LSECT:	LD      A,B             ; set track
-                OUT     (FDCT),A
-                LD      A,C             ; set sector
-                OUT     (FDCS),A
-                LD      A,L             ; set dma address low
-                OUT     (DMAL),A
-                LD      A,H             ; set dma adress high
-                OUT     (DMAH),A
-                LD		A,'.'
-                OUT		(CONDAT),A
-                XOR     A               ; read sector
-                OUT     (FDCOP),A
-                IN      A,(FDCST)       ; get status of fdc
-                OR      A               ; read successful ?
-                JR      NZ,BERR         ; no, stop
-                DEC     D               ; go to next sector if load is incomplete
-                JR      NZ,NEXT         ; head for the bios
-                LD		A,'+'
-                OUT		(CONDAT),A
-                JP		BIOS
+		OUT     (FDCT),A
+		LD      A,C             ; set sector
+		OUT     (FDCS),A
+		LD      A,L             ; set dma address low
+		OUT     (DMAL),A
+		LD      A,H             ; set dma adress high
+		OUT     (DMAH),A
+		LD		A,'.'
+		OUT		(CONDAT),A
+		XOR     A               ; read sector
+		OUT     (FDCOP),A
+		IN      A,(FDCST)       ; get status of fdc
+		OR      A               ; read successful ?
+		JR      NZ,BERR         ; no, stop
+		DEC     D               ; go to next sector if load is incomplete
+		JR      NZ,NEXT         ; head for the bios
+		LD		A,'+'
+		OUT		(CONDAT),A
+		JP		BIOS
 ;
 ;       more sectors to load
 ;
@@ -61,26 +61,26 @@ LSECT:	LD      A,B             ; set track
 ;             to hold the load address increment
 ;
 NEXT:	LD		A,128
-                ADD		A,L
-                LD		L,A
-                LD		A,H
-                ADC		A,0
-                LD		H,A				; <hl> = <hl> + 128
+		ADD		A,L
+		LD		L,A
+		LD		A,H
+		ADC		A,0
+		LD		H,A				; <hl> = <hl> + 128
 
 ;
-                INC     C               ; sector = sector + 1
-                LD      A,C
-                CP      27              ; last sector of track ?
-                JR      C,LSECT         ; no, go read another
+		INC     C               ; sector = sector + 1
+		LD      A,C
+		CP      27              ; last sector of track ?
+		JR      C,LSECT         ; no, go read another
 ;
 ;       end of track, increment to next track
 ;
-                LD      C,1             ; sector = 1
-                INC     B               ; track = track + 1
-                JR      LSECT           ; for another group
+		LD      C,1             ; sector = 1
+		INC     B               ; track = track + 1
+		JR      LSECT           ; for another group
 
 HALTMSG:DB	CR,LF,"Error",CR,LF
 BOOTMSG:DB	"BL V 1.0 boot:$"
-                DC  80h-$-2,0ffh
-                DW	NOT(MAGICID)
-        END	COLD							; of boot loader
+		DC  80h-$-2,0ffh
+		DW	NOT(MAGICID)
+		END	COLD							; of boot loader
